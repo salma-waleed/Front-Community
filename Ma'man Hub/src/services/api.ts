6a8d4ee7dfb260543547e39616188fc673d31815
Samarallam97 <samarshallam@gmail.com>
2026-05-06 11:31:13 +0300

    calendar component

--- Ma'man Hub/src/services/api.ts
+++ Ma'man Hub/src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://dashboard-maman.runasp.net/api";
-// const API_BASE_URL =import.meta.env.VITE_API_BASE_URL || "https://localhost:7220/api";
+// const API_BASE_URL =import.meta.env.VITE_API_BASE_URL || "https://dashboard-maman.runasp.net/api";
+const API_BASE_URL =import.meta.env.VITE_API_BASE_URL || "https://localhost:7220/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
